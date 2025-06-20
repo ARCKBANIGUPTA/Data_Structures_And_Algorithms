98c3a020ad815ceba8402a2d5f9ee4b26a6b4313
ARCKBANIGUPTA <arckbanigupta08@gmail.com>
2025-06-20 18:57:33 +0530

    Push data at the bottom of the stack

---
+++ Stacks/PushAtBottom.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class PushAtBottom {
+    public static void PushBottom(int data,Stack<Integer> s){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        PushBottom(data, s);
+        s.push(top);
+    }
+    
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        PushBottom(4, s);
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+
+    }
+}
