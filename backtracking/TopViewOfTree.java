b5b256e768735d1426d3483ca4de8766c06b0ea4
ARCKBANIGUPTA <arckbanigupta08@gmail.com>
2025-07-01 01:07:51 +0530

    Find the nodes which are visible from Top view of the tree

---
+++ backtracking/TopViewOfTree.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class TopViewOfTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class Info {
+        int hd;
+        Node node;
+
+        Info(Node node, int hd) {
+            this.hd = hd;
+            this.node = node;
+        }
+    }
+
+    public static void TopView(Node root) {
+        Queue<Info> q = new LinkedList<>();
+        HashMap<Integer, Node> hm = new HashMap<>();
+        int min = 0;
+        int max = 0;
+        q.add(new Info(root, 0));
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            Info curr = q.remove();
+            if (curr == null) {
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                if (!hm.containsKey(curr.hd)) {
+                    hm.put(curr.hd, curr.node);
+                }
+                if (curr.node.left != null) {
+                    q.add(new Info(curr.node.left, curr.hd - 1));
+                    min = Math.min(min, curr.hd - 1);
+                }
+                if (curr.node.right != null) {
+                    q.add(new Info(curr.node.right, curr.hd + 1));
+                    max = Math.max(max, curr.hd + 1);
+                }
+            }
+        }
+
+        for (int i = min; i <= max; i++) {
+            System.out.print(hm.get(i).data + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+        TopView(root);
+    }
+}
