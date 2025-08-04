2ba8af7ec1fee2fc5f40f4a63e88678a2219f841
ARCKBANIGUPTA <arckbanigupta08@gmail.com>
2025-08-04 17:34:58 +0530

    Find unique Substrings of a string

---
+++ Trie/UniqueSubstring.java
@@ -0,0 +1,63 @@
+public class UniqueSubstring {
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;// is the current letter is the end of word
+
+        Node(){
+           for(int i=0;i<26;i++){
+                children[i] = null;
+
+           } 
+        }
+
+    }
+    public static Node root = new Node();
+    public static void Addword(String word){
+        Node curr = root;
+        for(int level=0;level<word.length();level++){
+            int idx = word.charAt(level)-'a';
+            if(curr.children[idx]==null){
+                curr.children[idx] = new Node();
+
+            }
+            curr = curr.children[idx];
+
+        }
+        curr.eow=true;
+    }
+    public static boolean search(String word){
+        Node curr = root;
+        for(int i=0;i<word.length();i++){
+            int idx = word.charAt(i)-'a';
+            if(curr.children[idx]==null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow==true;
+    }
+
+    public static int countNode(Node root){
+        if(root==null){
+            return 0;
+        }
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(root.children[i]!=null){
+                count = count + countNode(root.children[i]);
+            }
+        }
+        return count+1;
+    }
+    public static void main(String[] args) {
+        String key = "apple";
+        //s1: find all suffixes
+        for(int i=0;i<key.length();i++){
+            String str = key.substring(i);
+            //s2: insert into trie
+            Addword(str);
+        }
+        System.out.println("Number of unique Substring are : "+countNode(root));
+
+    }
+}
