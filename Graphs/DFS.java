eff29ce08e65008fd46a3f620a83fba10d35f5e2
ARCKBANIGUPTA <arckbanigupta08@gmail.com>
2025-08-11 23:52:49 +0530

    Operations on Graphs

---
+++ Graphs/DFS.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+
+public class DFS {
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+        public Edge(int src,int dest,int wt){
+            this.src = src;
+            this.wt = wt;
+            this.dest = dest;
+        }
+    }
+
+
+
+    public static void creategraph(ArrayList<Edge> graph[]){
+        for(int i=0;i<graph.length;i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        // 0th vertex
+        graph[0].add(new Edge(0, 1, 4));
+        graph[0].add(new Edge(0, 2, 3));
+
+        // 1st vertex
+        graph[1].add(new Edge(1, 0, 4));
+        graph[1].add(new Edge(1, 3, 2));
+        graph[1].add(new Edge(1, 4, 7));
+
+        // 2nd vertex
+        graph[2].add(new Edge(2, 0, 3));
+        graph[2].add(new Edge(2, 4, 4));
+
+        // 3rd vertex
+        graph[3].add(new Edge(3, 1, 2));
+        graph[3].add(new Edge(3, 5, 1));
+
+        // 4th vertex
+        graph[4].add(new Edge(4, 1, 7));
+        graph[4].add(new Edge(4, 2, 4));
+        graph[4].add(new Edge(4, 5, 3));
+        graph[4].add(new Edge(4, 6, 5));
+
+        // 5th vertex
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 3));
+        graph[5].add(new Edge(5, 6, 2));
+
+        // 6th vertex
+        graph[6].add(new Edge(6, 4, 5));
+        graph[6].add(new Edge(6, 5, 2));
+    }
+
+   public static void dfs(ArrayList<Edge>[] graph, int curr, boolean[] vis) {
+    // Mark the current node as visited
+    vis[curr] = true;
+    System.out.print(curr + " ");
+
+    // Traverse all edges from the current node
+    for (int i = 0; i < graph[curr].size(); i++) {
+        Edge e = graph[curr].get(i);
+        if (!vis[e.dest]) {
+            dfs(graph, e.dest, vis);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int V = 7;
+        ArrayList<Edge>[] graph = new ArrayList[V];//->null arraylist
+        creategraph(graph);
+        
+        dfs(graph, 0, new boolean[V]);
+    }
+
+}
