428cddf8d43291dc75b1cd9720756793b02ce518
ARCKBANIGUPTA <arckbanigupta08@gmail.com>
2025-08-29 12:38:35 +0530

    find the minimum cost for matrix chain multiplication

--- DynamicProgramming/Matrix_Chain.java
+++ DynamicProgramming/Matrix_Chain.java
@@ -17,6 +17,27 @@ public class Matrix_Chain {
         }
         return dp[i][j] = ans;
     }
+    public static int MCM_Tabulation(int arr[]){
+        int n=arr.length;
+        int dp[][] = new int[n][n];
+        for(int i=0;i<n;i++){
+            dp[i][i]=0;
+        }
+        
+        for(int len=2;len<=n-1;len++){
+            for(int i=1;i<=n-len;i++){//row
+                int j=i+(len-1);//col
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k=i;k<=j-1;k++){
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k+1][j];
+                    int cost3 = arr[i-1]*arr[k]*arr[j];
+                    dp[i][j] = Math.min(dp[i][j], cost1+cost2+cost3);
+                }
+            }
+        }
+        return dp[1][n-1];
+    }
 
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,3};
@@ -27,6 +48,7 @@ public class Matrix_Chain {
                 dp[i][j] = -1;
             }
         }
-        System.out.println(MCM_Memoization(arr, 1, n-1,dp));
+        System.out.println("MINIMUM COST<MEMOIZATION>"+MCM_Memoization(arr, 1, n-1,dp));
+        System.out.println("MINIMUM COST<TABULATION>"+MCM_Tabulation(arr));
     }
 }
